5cfe06b932d77db3979603886b164302b21b3c07
niubir <niubir>
2025-09-17 19:26:55 +0800

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+function hello(name) {
+  return `Hello, ${name}!`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { hello, add };
---
+++ test.js
@@ -0,0 +1,4 @@
+const myutil = require("./index");
+
+console.log(myutil.hello("Zhan")); // Hello, Zhan!
+console.log(myutil.add(2, 3));    // 5
